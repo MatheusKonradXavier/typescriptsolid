fbed76f3a025ae4364b47e8de4d3e9ed6d758976
Matheus Konrad <matheuskonradxavier@hotmail.com>
2022-02-17 11:55:50 -0300

    Fim do modulo SOLID

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-const nome = 'A';
-console.log(nome);
+/*
+
+Vantagens e desvantagens do S.O.L.I.D
+
+Vantagens:
+- Código modular
+- Código reutilizável (D.R.Y - Don't repeat yourself)
+- Código testável, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Código expansível
+- Separations of concerns (Separação de conceitos)
+- Fácil manutenção
+
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tenha cuidados com:
+  * YAGNI (You aren't gonna need it),
+  * KISS (Keep it simple, stupid!)
+- Tempo de desenvolvimento aumenta bastante
+
+*/
